06b31b35c3b77f25c314ff18d50d85d7ecd1f6ee
Arto <myemail>
2023-07-06 22:45:10 -0300

    feat: teste com url npm specifier

--- teste.js
+++ teste.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import fetch from 'npm:node-fetch@3.3.1';
 export default async ({ params, response }) => {
     const dados = await fetch('https://api.github.com/users/github');
     const resposta = await dados.json();
